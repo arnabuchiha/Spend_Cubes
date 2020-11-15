9150f6806fbfc631124cee5084c4ef66d07b8ce6
arnabuchiha <arnabkeya04@gmail.com>
2020-11-15 16:23:49 +0530

    Blogs Done

--- src/components/Cards/BlogCard.js
+++ src/components/Cards/BlogCard.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const BlogCard=({image,title,author,date,topic})=>{
     return(
-        <div class="col-xl-6">
-        <div className="latestBlog-wrapper">
-            <div class="latest-img">
+        <div class="col-xl-4">
+        <div className="blog-wrapper">
+            <div class="blog-img">
                 <img src={image} alt=""/>
             </div>
-            <div class="latest-content">
+            <div class="blog-content">
                     <h3> <a href="#">{title}</a> </h3>        
             </div>
             <div class="slider-date">
