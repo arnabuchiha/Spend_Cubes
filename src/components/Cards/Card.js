350e72c513c3902d8519b2383ce78759d927b60b
arnabuchiha <arnabkeya04@gmail.com>
2020-11-25 09:29:23 +0530

    Home changes

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../../assets/styles/card.css"
 const Card=({image,name,position,about,linkedin})=>{
     return(
-        <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
         <div class="image-flip" >
                 <div class="mainflip flip-0">
                     <div class="frontside">
@@ -10,7 +10,7 @@ const Card=({image,name,position,about,linkedin})=>{
                     <img class="card-img-top" src={image} alt="Card image cap"/>
                         <div class="card-body">
                         <h5 class="card-title">{name}</h5>
-                        <h6 class="card-title">{position}</h6>
+                        <p class="card-title">{position}</p>
                         </div>
                     </div>
                 </div>
