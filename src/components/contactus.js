db90a45b16a7c85d4904de752d925474a2553d21
arnabuchiha <arnabkeya04@gmail.com>
2020-12-27 18:33:10 +0530

    social-media links added and team card fixed

--- src/components/contactus.js
+++ src/components/contactus.js
@@ -76,11 +76,13 @@ class Contact extends React.Component{
                                     <a href="mailto: business@spendcubes.com">business@spendcubes.com</a>
                                 </div>
                             </div>
+                            <hr></hr>
                             <div className="d-flex contactus-par-ele">
+                                
                                 <div class="social-footer-icons">
                                     <ul class="d-inline">
-                                    <li><a href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                    <li><a href="https://twitter.com/?lang=en"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                    <li><a href="https://www.facebook.com/SpendCubes/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                    <li><a href="https://www.linkedin.com/company/spendcubes/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
