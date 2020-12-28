9442cd27cffb2ca20c687b212abbf86feb0fec0c
arnabuchiha <arnabkeya04@gmail.com>
2020-12-28 17:18:31 +0530

    career page redesigned

---
+++ src/components/career/JobsCard.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../../assets/styles/jobscard.css";
+
+class JobsCard extends React.Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div>
+                <div className="container jobscard mb-4 ">
+                    <div className="row">
+                        <div className="col">
+                            <p className="row location">
+                                Laxmi Nagar,Delhi
+                            </p>
+                            <div className="row">
+                                <h2>Analyst</h2>
+                            </div>
+                            <p className="row depart">
+                                Telecom Expense Management / KPO
+                            </p>
+                        </div>
+                        <div className="col mt-auto mb-auto text-right">
+                            <a href="mailto:careers@spendcubes.com?subject=ANALYST20200901" target="_blank" className="btn btn-primary apply">APPLY</a>
+                            <button className="btn btn-primary">MORE INFO</button>
+                        </div>
+                        
+                    </div>
+                    {/* <hr className="row end-line"></hr> */}
+                </div>
+            </div>
+        )
+    }
+}
+export default JobsCard;
